a00230506a15433534f9045290d0e5c547868d47
BOB <zhubo0228@163.com>
2016-03-08 14:36:28 +0800

    Initial commit

---
+++ app/src/main/java/com/namilili/bob/memorytest/MainActivity.java
@@ -0,0 +1,81 @@
+package com.namilili.bob.memorytest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<String> myData = new ArrayList<String>();
+    private TextView myTextview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        myTextview = (TextView) this.findViewById(R.id.myTextView);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void addDataClickHandle(View view)
+    {
+        for (int i = 0; i < 1000; i++) {
+            myData.add("value " + i );
+        }
+        showDataSize();
+    }
+
+    public void clearDataClickHandle(View view)
+    {
+        myData.clear();
+        showDataSize();
+
+    }
+
+    private void showDataSize()
+    {
+        myTextview.setText("Number of data items : "+myData.size());
+    }
+
+}
